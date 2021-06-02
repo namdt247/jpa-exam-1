e9fba7c620c5c072585ecb88207d61fec45f7b7b
Dinh Nam <nam.dt247@gmail.com>
2021-06-02 20:18:42 +0700

    update home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import List from "./components/List";
+import {Col, Row} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Create from "./components/Create";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-          <Route exact path="/list">
-              <List />
-          </Route>
-          <Route exact path="/create">
-              <Create />
-          </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Row className='mt-4'>
+                    <Col xs='auto'>
+                        <Link to="/list">
+                            Danh sách nhân viên
+                        </Link>
+                    </Col>
+                    <Col xs='auto' className="mx-3">
+                        <Link to="/create">
+                            Thêm mới nhân viên
+                        </Link>
+                    </Col>
+                </Row>
+            </div>
+
+            <Switch>
+                <Route exact path="/list">
+                    <List/>
+                </Route>
+                <Route exact path="/create">
+                    <Create/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
